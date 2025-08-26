f2853841ccaa8a699182fbacb7654255efe2647d
Rami <128874824+rprado22@users.noreply.github.com>
2025-08-25 20:22:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+export default defineConfig({
+  base: "/ratios-simulator/",
+  plugins: [react()],
+})
 export default defineConfig({
 plugins: [react()],
 })
